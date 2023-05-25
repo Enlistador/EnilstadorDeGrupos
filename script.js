03849bf159cf7152611ee4f565cdf4dea46c1b47
Enlistador <134576904+Enlistador@users.noreply.github.com>
2023-05-25 15:48:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,69 @@
-let currid = 0;
 const objetos = [];
+let currentEdit = -1
+
+
+function modifyTable(id)
+{
+  if(currentEdit != -1) 
+  {
+    alert("Ya estas editando una tabla, guardala para poder editar otra")
+    return;
+  }
+    objetos.forEach((objeto, index) => {
+    if (objeto.id == id) {
+      objeto.edited = true
+      document.getElementById('nuevo-dia').value = objeto.dia;
+      document.getElementById('nueva-hora').value = objeto.hora;
+      document.getElementById('nuevo-lugar').value = objeto.lugar;
+      document.getElementById('nuevo-territorio').value = objeto.territorio;
+      document.getElementById('nuevo-conductor').value = objeto.conductor;
+    
+      currentEdit = objeto.id;
+
+      addRowBtn.classList.add("hiddenRow")
+      editRowBtn.classList.remove("hiddenRow")
+    
+    }
+  });
+  cargarTabla();
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+
+function saveEdit()
+{
+  if(currentEdit == -1) return false;
+  objetos.forEach(objeto => {
+    if (objeto.id == currentEdit) {
+
+
+      objeto.edited = false
+      objeto.dia = document.getElementById('nuevo-dia').value;
+      objeto.hora = document.getElementById('nueva-hora').value;
+      objeto.lugar = document.getElementById('nuevo-lugar').value;
+      objeto.territorio = document.getElementById('nuevo-territorio').value;
+      objeto.conductor = document.getElementById('nuevo-conductor').value;
+
+
+    }
+  });
+
+  currentEdit = -1
+
+  document.getElementById('nuevo-dia').value = '';
+  document.getElementById('nueva-hora').value = '';
+  document.getElementById('nuevo-lugar').value = '';
+  document.getElementById('nuevo-territorio').value = '';
+  document.getElementById('nuevo-conductor').value = '';
+
+  addRowBtn.classList.remove("hiddenRow")
+  editRowBtn.classList.add("hiddenRow")
+
+  cargarTabla()
+
+  return true;
+}
+
 
 function borrarTabla(id) {
   // Buscar y eliminar el objeto con el ID especificado
@@ -15,6 +79,7 @@ function cargarTabla() {
   const tablaBody = document.getElementById('tabla-body');
   tablaBody.innerHTML = '';
 
+  let currid = 0;
   let variant = true
 
   const desde = document.getElementById('desde').value;
@@ -30,16 +95,20 @@ function cargarTabla() {
       <td>${objeto.lugar}</td>
       <td>${objeto.territorio}</td>
       <td>${objeto.conductor}</td>
-      <td class="deleteTable"><button class="deleteBtn" onclick="borrarTabla(${objeto.id})"><i class="fa-solid fa-trash"></i></button></td>
+      <td class="deleteTable"><button class="deleteBtn" onclick="borrarTabla(${objeto.id})"><i class="fa-solid fa-trash"></i></button><button class="editBtn" onclick="modifyTable(${objeto.id})"><i class="fa-solid fa-pen-to-square"></i></button></td>
     `;
     currid++;
     if(variant) row.classList.add("variant");
+    if(objeto.edited) row.classList.add("hiddenRow");
     variant = !variant
     tablaBody.appendChild(row);
   });
 }
 
 function agregarObjeto() {
+
+  if(saveEdit()) return;
+
   const nuevoDia = document.getElementById('nuevo-dia').value;
   const nuevaHora = document.getElementById('nueva-hora').value;
   const nuevoLugar = document.getElementById('nuevo-lugar').value;
@@ -51,7 +120,8 @@ function agregarObjeto() {
     hora: nuevaHora,
     lugar: nuevoLugar,
     territorio: nuevoTerritorio,
-    conductor: nuevoConductor
+    conductor: nuevoConductor,
+    edited: false
   };
 
   objetos.push(nuevoObjeto);
